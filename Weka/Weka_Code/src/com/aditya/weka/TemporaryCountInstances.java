487cd2c9e58defe9ad6c2d40ad573f7a31ee0d4c
skymanaditya1 <skymanaditya1@gmail.com>
2015-09-02 14:00:22 +0530

    A file was deleted

--- Weka/Weka_Code/src/com/aditya/weka/TemporaryCountInstances.java
+++ Weka/Weka_Code/src/com/aditya/weka/TemporaryCountInstances.java
@@ -14,13 +14,14 @@ public class TemporaryCountInstances {
 	public static final String file_name = "/home/aditya/CCBD_Sound_Internship/Sound1/Airplane/1.arff";
 	
 	public static void main(String[] args) throws Exception{
-		DataSource source = new DataSource(file_name);
+		/**DataSource source = new DataSource(file_name);
 		Instances dataset = source.getDataSet();
 		
 		// Get the number of Instances in the given file
 		int num_Instances = dataset.numInstances();
 		
 		System.out.println("The number of Instances in the given file is "+num_Instances);
+		*/
 		
 	}
 }
